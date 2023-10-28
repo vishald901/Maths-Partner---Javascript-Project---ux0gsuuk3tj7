45baf6cf82c26d960f6efe067ed00908e8b2efd2
Vishal Dubey <120295697+vishald901@users.noreply.github.com>
2023-10-28 12:22:28 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,78 +1,21 @@
-console.log("hello");
+function updateCalendar() {
+    const month = document.getElementById("month").value;
+    const year = document.getElementById("year").value;
 
+    // Send a POST request to your PHP server with month and year.
+    // Process the response to generate the calendar and update #calendar-container.
+    // You need server-side code to generate the calendar based on your data.
 
-function SavedButton(){
-    console.log("working save btn")
-    var wet =  document.querySelector(".inputBox1").innerHTML;
-    var set =  document.querySelector(".inputBox2").innerHTML;
-
-    localStorage.setItem(wet,set);
-    console.log(localStorage.getItem(set));
-    set++;
+    // Example:
+    // const calendarHTML = "<div class='cell'>1</div><div class='cell'>2</div>..."; 
+    // document.getElementById("calendar-container").innerHTML = calendarHTML;
 }
 
-var hcount = 0 
-function historyButton(){
-
-    console.log("working history btn")
-    hcount++;
-    if(hcount%2==1){
-        document.querySelector(".historycon").style.display = "block";
-        var data = document.querySelector('.historycon');
-
-        for (p in localStorage) {
-            var ans = localStorage.getItem(p);
-            if (ans != null) {
-                // console.log(ans);
-                var h3 = document.createElement("h3");
-                h3.setAttribute('class', 'remove')
-
-                h3.innerHTML = `${p} => Result = ${ans}`;
-                console.log(h3);
-                data.appendChild(h3);
-
-            }
-        }
-    }else{
-        document.querySelector(".historycon").style.display = "none";
-        var rem = document.querySelectorAll('.remove');
-        console.log(rem);
-        for (var i = 0; i < rem.length; i++) {
-            rem[i].remove();
-        }
+function changeCellColor(event) {
+    if (event.key === "Enter") {
+        const inputDate = parseInt(document.getElementById("date-input").value);
+        // Send a POST request to your PHP server with the inputDate.
+        // Update the cell background color based on the response from the server.
+        // You need server-side code to handle this and store the last background color in PHP session.
     }
 }
-
-function SearchButton(){
-    event.preventDefault();
-
-    console.log("working search btn")
-   var problem = document.getElementById('problemBar').value;
-   var category = document.getElementById('categoryBar').value;
-
-   let pro = fetch(`https://newton.vercel.app/api/v2/${category}/${problem}`)
-   console.log(pro)
-   pro.then((reponse) => {
-    console.log(reponse.status)
-    console.log(reponse.ok)
-    return reponse.json()
-   }).then((value) => {
-    console.log(value);
-    var input2 =  value.result;
-    document.querySelector(".inputBox2").innerHTML = input2;
-   })
-   
-
-   const Input1  = document.querySelector(".inputBox1").innerHTML = category+" :"+problem;
-   var set =  document.querySelector(".inputBox1").value;
-   var wet =  document.querySelector(".inputBox2").innerHTML;
-   localStorage.setItem(set,wet);
-}
-
-
-function delBtn(ele){
-    console.log("working delete btn")
-    document.querySelector(".inputBox1").innerHTML = "";
-    document.querySelector(".inputBox2").innerHTML = "";
-}
-
